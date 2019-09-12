8ececa8896999056ae9c2abb0ef3691a82a65a74
SerhiyTrots <55109509+SerhiyTrots@users.noreply.github.com>
2019-09-12 18:11:34 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include<array>
+using namespace std;
+
+int main()
+{ 
+	int arr[5] = { 1,2,3,4,5 };
+	int even = arr[0] + arr[1];
+
+	system("pause");
+	return 0;
+}
+ 
